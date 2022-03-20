b9d447d7de812a6d61d766381349f44fd2fb7bd6
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-03-20 21:10:30 +0800

    functions

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 	"strings"
 )
 
+// here is package level so can be assesed anywhere
+var conferenceName = "Go Conference" // to create variables use var prefix to tell go that this is a variable
+// a different way to create a var we can use := to replace var, but in this case we cant fix its type
+const conferenceTickets = 50 // for constants in the code, we add prefix const
+var remainingTickets uint = 50
+
 func main() { // this is where it tells the compiler the code starts here only 1 main func in go file
-	var conferenceName = "Go Conference" // to create variables use var prefix to tell go that this is a variable
-	// a different way to create a var we can use := to replace var, but in this case we cant fix its type
-	const conferenceTickets = 50 // for constants in the code, we add prefix const
-	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceaName is %T\n", conferenceName, conferenceTickets, remainingTickets) // to voew ots ty[e ]
 	// fmt.Print("Hello World") // Print on its own does not exist, thats why fmt needs to be imported for it to work
 	// fmt.Println("Hello World with line below")
-	fmt.Println("Welcome to", conferenceName, "booking application") // space gets added automatically
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
-	fmt.Println("Get your tickets here to attend")
+	greetUsers()
 
 	// instead of typing it out like above, we can use template string
 	fmt.Println("")
@@ -38,51 +38,102 @@ func main() { // this is where it tells the compiler the code starts here only 1
 	//adding new elements
 
 	// for loop time
-	for { // indefinite loop 
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		// ask user for their name
-		// in go, it requires hard typing
-		// two basic data types
-		// String for textual
-		// Integers for whole numbers but there are different types of numeric data types
-		// If we assign a value when variable is created, go can infer
-		// else we need to tell explicitly typed
+	for { // indefinite loop
+		// for remainingTickets > 0 { // we can add a condition to make it work in indefinite loop
 
-		// pointer points to memory address
-		// fmt.Println(&remainingTickets) // this gives the memory address
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTickets := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-		fmt.Println("Enter your first name")
-		fmt.Scan(&firstName) // get value from user input by waiting for input
-		fmt.Println("Enter your last name")
-		fmt.Scan(&lastName) // get value from user input by waiting for input
-		fmt.Println("Enter your email")
-		fmt.Scan(&email) // get value from user input by waiting for input
-		fmt.Println("Enter number of tickets")
-		fmt.Scan(&userTickets) // get value from user input by waiting for input
-
-		remainingTickets = remainingTickets - userTickets
-		// bookings[0] = firstName + " " + lastName // for arary
-		bookings = append(bookings, firstName+" "+lastName)
-
-		// fmt.Printf("The whole array: %v\n", bookings)
-		// fmt.Printf("The first value: %v\n", bookings[0])
-		// fmt.Printf("Array Type: %T\n", bookings)
-		// fmt.Printf("Array length: %v\n", len(bookings))
-
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n ", firstName, lastName, userTickets, email)
-		fmt.Printf("%v remaining from %v\n", remainingTickets, conferenceName)
-
-		firstNames := []string{}
-		for index, booking := range bookings {// range is needed to iterate over elements 
-			var strings.Fields(booking) // split strings with white space
-			var firstName = names[0] 
+		if isValidName && isValidEmail && isValidTickets {
+			bookTickets(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
+			if remainingTickets == 0 {
+				// end the program here
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break // breaks the indefinite loop
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name is too short ")
+			}
+			if !isValidEmail {
+				fmt.Println("Email entered is not valid")
+			}
+			if !isValidTickets {
+				fmt.Println("Number of ticekts you entered is invalid")
+			}
+			// fmt.Printf("Your input data is invalid pls try again.\n", remainingTickets, userTickets)
+			// continue // instead of breaking out, skip to the next iteration, not needed if in else block
 		}
-			fmt.Printf("These are the list of bookers %v\n", bookings)
 	}
 
 }
 
+func greetUsers() { // need to tell function what type the variable is gonna be
+	// fmt.Printf("Welcome to %v booking application\n", confName)
+	fmt.Printf("Welcome to %v booking application", conferenceName) // space gets added automatically
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
 // to execute, use { go run <file name>}
+func printFirstNames(bookings []string) []string { // need to define type of returned values
+	firstNames := []string{}
+	for _, booking := range bookings { // range is needed to iterate over elements, _ is used to replace unused variables, index is in its place range gives index and value
+		var names = strings.Fields(booking) // split strings with white space
+		var firstName = names[0]
+		firstNames = append(firstNames, firstName)
+	}
+	return firstNames
+}
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTickets := userTickets > 0 && userTickets < remainingTickets
+
+	return isValidName, isValidEmail, isValidTickets
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+	// in go, it requires hard typing
+	// two basic data types
+	// String for textual
+	// Integers for whole numbers but there are different types of numeric data types
+	// If we assign a value when variable is created, go can infer
+	// else we need to tell explicitly typed
+
+	// pointer points to memory address
+	// fmt.Println(&remainingTickets) // this gives the memory address
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName) // get value from user input by waiting for input
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName) // get value from user input by waiting for input
+	fmt.Println("Enter your email")
+	fmt.Scan(&email) // get value from user input by waiting for input
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets) // get value from user input by waiting for input
+	return firstName, lastName, email, userTickets
+}
+
+func bookTickets(remainingTickets uint, userTickets uint, bookings []string, firstName string, lastName string, email string, conferenceName string) {
+	remainingTickets = remainingTickets - userTickets
+	// bookings[0] = firstName + " " + lastName // for arary
+	bookings = append(bookings, firstName+" "+lastName)
+
+	// fmt.Printf("The whole array: %v\n", bookings)
+	// fmt.Printf("The first value: %v\n", bookings[0])
+	// fmt.Printf("Array Type: %T\n", bookings)
+	// fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n ", firstName, lastName, userTickets, email)
+	fmt.Printf("%v remaining from %v\n", remainingTickets, conferenceName)
+
+	firstNames := printFirstNames(bookings)
+	fmt.Printf("These are the list of bookers %v\n", firstNames)
+}
