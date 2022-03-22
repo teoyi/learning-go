1f08acb4921e214cc4e32f5c157874c3c51467d5
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-03-22 18:01:24 +0800

    Completed tutorials

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ package main // all code needs to belong to a package.
 import (
 	"booking-app/helper"
 	"fmt" // just like python libraries needs to be imported fmt - format
-	"strconv"
+	"sync"
+	"time"
 )
 
 // here is package level so can be assesed anywhere
@@ -14,7 +15,18 @@ const conferenceTickets = 50 // for constants in the code, we add prefix const
 var remainingTickets uint = 50
 
 // instead of a list of string we want a map
-var bookings = make([]map[string]string, 0) // to create an empty slice of maps, we need to indicate the starting size. It is dynamic so putting 0 works
+// var bookings = make([]map[string]string, 0) // to create an empty slice of maps, we need to indicate the starting size. It is dynamic so putting 0 works
+
+var bookings = make([]UserData, 0) // using struct for our bookings
+// TO DEAL WITH MULTI TYPES  we use struct
+type UserData struct { // this creates a custom type named UserData
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() { // this is where it tells the compiler the code starts here only 1 main func in go file
 
@@ -51,7 +63,10 @@ func main() { // this is where it tells the compiler the code starts here only 1
 		isValidName, isValidEmail, isValidTickets := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTickets {
-			bookTickets(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
+			bookTickets(remainingTickets, userTickets, firstName, lastName, email)
+			// to tackle the iossue of main thread not waiting for other threads we use waitgroup
+			wg.Add(1)                                               // number here depends on number of threads to be added to the wait group. If more than 1 go exists, we +1 the nuumber
+			go sendTickets(userTickets, firstName, lastName, email) // go starts a go routine and introduce concurrency however main thread does not wait for other threads to finish their work to end the aplpication
 			if remainingTickets == 0 {
 				// end the program here
 				fmt.Println("Our conference is booked out. Come back next year.")
@@ -71,7 +86,7 @@ func main() { // this is where it tells the compiler the code starts here only 1
 			// continue // instead of breaking out, skip to the next iteration, not needed if in else block
 		}
 	}
-
+	wg.Wait() // this tells main thread to wait for threads in wait group to be done so add done command at end of logic
 }
 
 func greetUsers() { // need to tell function what type the variable is gonna be
@@ -82,12 +97,12 @@ func greetUsers() { // need to tell function what type the variable is gonna be
 }
 
 // to execute, use { go run <file name>}
-func printFirstNames(bookings []map[string]string) []string { // need to define type of returned values
+func printFirstNames() []string { // need to define type of returned values
 	firstNames := []string{}
 	for _, booking := range bookings { // range is needed to iterate over elements, _ is used to replace unused variables, index is in its place range gives index and value
 		// var names = strings.Fields(booking) // split strings with white space
 		// var firstName = names[0]
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -119,18 +134,24 @@ func getUserInput() (string, string, string, uint) {
 	return firstName, lastName, email, userTickets
 }
 
-func bookTickets(remainingTickets uint, userTickets uint, bookings []map[string]string, firstName string, lastName string, email string, conferenceName string) {
+func bookTickets(remainingTickets uint, userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
 	// bookings[0] = firstName + " " + lastName // for arary
 
 	// create a map for a user
-	var userData = make(map[string]string) //  craete empty map and their types. Inside brackets is key outside is value. Make is built in to tell it to create a map cannot mix data types in map
+	// var userData = make(map[string]string) //  craete empty map and their types. Inside brackets is key outside is value. Make is built in to tell it to create a map cannot mix data types in map
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
 
 	// to add value into map
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10) // formatUint takes in a value and formats it into a decimal number, second argument is base 10
+	// userData["firstName"] = firstName
+	// userData["lastName"] = lastName
+	// userData["email"] = email
+	// userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10) // formatUint takes in a value and formats it into a decimal number, second argument is base 10
 
 	// bookings = append(bookings, firstName+" "+lastName)
 	bookings = append(bookings, userData)
@@ -143,6 +164,14 @@ func bookTickets(remainingTickets uint, userTickets uint, bookings []map[string]
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n ", firstName, lastName, userTickets, email)
 	fmt.Printf("%v remaining from %v\n", remainingTickets, conferenceName)
 
-	firstNames := printFirstNames(bookings)
+	firstNames := printFirstNames()
 	fmt.Printf("These are the list of bookers %v\n", firstNames)
+}
+
+func sendTickets(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)                                                       // to simulate that this function takes time to run
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName) //sprintf allows asving to var
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
+	fmt.Println("##########################################")
+	wg.Done()
 }
