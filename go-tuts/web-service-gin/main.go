52637c2cb930de0440277718388e717d9b35be66
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-03-29 21:35:37 +0800

    completed simple restful api

--- go-tuts/web-service-gin/main.go
+++ go-tuts/web-service-gin/main.go
@@ -30,8 +30,38 @@ var albums = []album{
 }
 
 func main() {
-	router := gin.Default()          // establish a gin router using default
-	router.GET("/albums", getAlbums) // associate get function with the /albums path. NOTE: no need for () just the name will do
+	router := gin.Default()                 // establish a gin router using default
+	router.GET("/albums", getAlbums)        // associate get function with the /albums path. NOTE: no need for () just the name will do
+	router.GET("/albums/:id", getAlbumByID) // if colon, represents path parameter
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080") // Attach the router to a server
+}
+
+// postAlbums adds an album from JSOn received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSOn to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id") // get id from param
+
+	// Loop over the list of albums, looking for an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
