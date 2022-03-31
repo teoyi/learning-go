aedda5b474d0a46d57697f7629eb20b6ca5cede1
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-03-31 21:37:08 +0800

    completed fuzz

--- go-tuts/fuzz/main.go
+++ go-tuts/fuzz/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
 	input := "The quick brown fox jumped over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
 	fmt.Printf("original: %q\n", input)
-	fmt.Printf("reversed: %q\n", rev)
-	fmt.Printf("reversed again: %q\n", doubleRev)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
 }
 
 // Reverse will accept a string, loop over it a byte at a time, and return the reversed string at the end.
 // Similar to reverse function in stringutil
-func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("Input is not valid UTF-8")
 	}
-	return string(b)
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
 }
--- go-tuts/fuzz/reverse_test.go
+++ go-tuts/fuzz/reverse_test.go
@@ -1,26 +1,50 @@
-package main 
+package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-// this is a unit test 
-func TestReverse(t *testing.T) {
-	testcases := []struct { 
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-        {" ", " "},
-        {"!12345", "54321!"},
-	}
+// this is a unit test
+// func TestReverse(t *testing.T) {
+// 	testcases := []struct {
+// 		in, want string
+// 	}{
+// 		{"Hello, world", "dlrow ,olleH"},
+// 		{" ", " "},
+// 		{"!12345", "54321!"},
+// 	}
 
-	for _,tc := range testcases { 
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+// 	for _, tc := range testcases {
+// 		rev, := Reverse(tc.in)
+// 		if rev != tc.want {
+// 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+// 		}
+// 	}
+// }
+
+// limits of unit test is that each input must be added to test by the dev
+// Fuzzing comes up with inputs for the code and may identify edge cases that the test cases the dev comes up with didnt reach .
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc) // use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
 		}
-	}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
+		// t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		if orig != doubleRev {
+			t.Errorf("Before: %q, After: %q", orig, doubleRev) // since random values are being inputted and cant be controlled, we reverse it twice to get back the original string
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invlaid UTF-8 string %q", rev) // we can test that the reversed string still preserves its state as a valid UTF-8
+		}
+	})
 }
-
-// limits of unit test is that each input must be added to test by the dev 
-// Fuzzing comes up with inputs for the code and may identify edge cases that the test cases the dev comes up with didnt reach . 
